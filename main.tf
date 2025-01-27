72de4fb94bd305894a64f206951db15adb5c96e0
root <root@sunny-test-cicd.us-central1-b.c.daring-keep-446309-u4.internal>
2025-01-27 13:31:34 +0000

    Updated Terraform configuration

--- main.tf
+++ main.tf
@@ -1,11 +1,7 @@
 # Google reources
 
 resource "google_storage_bucket" "bucket" {
-<<<<<<< HEAD
   project  = "daring-keep-446309-u4"
-=======
-  project  = "dl-k8s-dev1cade"
->>>>>>> sm
   name     = "daslearning-bucket-test"
   location = "ASIA-SOUTH1"
 }
